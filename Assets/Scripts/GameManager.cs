9ee348e24fc477794967e7136557a7811a874156
chernandez-git <chernandez@digitalgeko.com>
2015-12-07 23:25:37 -0600

    Spawning Creeps
    Added Creep Prefab
    Added Player Sprite
    Added Button mapping script.
    Added Game Manager script
    Added SpawnCreen scene to test Spawn related functionality

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public GameObject creep;
+	public Vector2 spawnValues;
+	public int creepCount;
+	void Start (){
+		SpawnCreeps ();
+	}
+	void SpawnCreeps(){
+		for (int i=0;i<creepCount;i++) {
+			Vector2 spawnPosition = new Vector2 (5, 5);
+			Quaternion spawnRotation = new Quaternion ();
+			Instantiate (creep, spawnPosition, spawnRotation);
+		}
+	}
+}
